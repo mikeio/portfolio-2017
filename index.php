a6939bf87def397b7a69cd716bfc69b845176198
Mike Peters <mapdesigns@gmail.com>
2016-12-30 11:03:55 -0500

    start index page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
+    <title>The Online Portfolio of Mike Peters</title>
 
     <!--Default Styles-->
     <link rel="stylesheet" href="css/foundation.css">
@@ -12,20 +12,30 @@
     <!--Custom Styles-->
     <link rel="stylesheet" href="css/app.css">
 
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#666666">
+    <meta name="theme-color" content="#212121">
+
   </head>
   <body>
 
-  <div class="overlay">
-    <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
-  </div>
+
+    <div class="overlay">
+      <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+    </div>
+
 
     <header>
 
       <div class="row">
         <div class="large-12 columns">
-
           <div class="top-bar">
-            <img src="img/logo.svg" id="logo">
+            <img src="img/logo.svg" class="logo">
             <button type="button" class="nav-toggle" aria-label="toggle navigation">
               <span class="nav-label">Menu</span>
               <span class="nav-svg">
@@ -38,32 +48,221 @@
               </span>
             </button>
           </div>
-
         </div>
       </div>
 
       <div class="row">
         <div class="large-12 columns">
-
           <div class="intro">
-            <h4>Hello! My name is <span>Mike Peters</span></h4>
-            <h1>I’m a <span>senior digital designer</span> and coder based in Atlanta, GA.</h1>
+            <h4>Hello! My name is Mike&nbsp;Peters</h4>
+            <h1>I’m a <span>senior digital designer</span> and coder based in Atlanta,&nbsp;GA.</h1>
           </div>
-
         </div>
       </div>
 
     </header>
 
-    <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
 
-    <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+    <section class="about-me" id="about">
 
-    <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+      <div class="row">
+        <div class="large-12 columns">
+          <img src="img/books.png">
+          <h2>About Me</h2>
+          <h4>Various Aspects</h4>
+          <h1>An eye for innovation and a drive to create beautiful & simple solutions.</h1>
+          <p>I’m a highly-motivated and dedicated professional with almost a decade of commercial design experience. This experience together with freelance work I've done, has allowed me to acquire a wide range of skills as a designer. Building products from the start, working side by side with developers and learning how users interact with my designs is what I love the most about my job. I believe good design is a simple and achievable experience that differs with each and every project I work on.</p>
+        </div>
+      </div>
 
-    <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+    </section>
 
-    <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+
+    <section class="featured-projects" id="featured">
+
+      <div class="row">
+        <div class="large-12 columns">
+          <h1>Featured Projects</h1>
+          <h4>What's In The Works</h4>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1000/600/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1001/601/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1002/602/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1003/603/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+      </div>
+
+      <div class="row" id="hidden">
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1000/600/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1001/601/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1002/602/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+
+        <div class="large-6 columns">
+          <figure class="project-list">
+            <img src="https://unsplash.it/1003/603/?random">
+            <figcaption>
+              <h1>Project Title</h1>
+              <h4>Subtitle</h4>
+            </figcaption>
+            <a href="#"></a>
+          </figure>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="large-12 columns">
+          <a class="btn btn-light" id="view-all">View All Projects</a>
+        </div>
+      </div>
+
+    </section>
+
+
+    <section class="my-resume" id="resume">
+
+      <div class="row">
+        <div class="large-12 columns">
+          <h1>Want to learn more?</h1>
+          <h4>View My Resume</h4>
+          <a class="btn btn-dark" href="#">Download PDF</a>
+        </div>
+      </div>
+
+    </section>
+
+
+    <footer>
+
+      <div class="row">
+        <div class="large-12 columns">
+          <ul class="navigation">
+            <li><a href="/">Home</a></li>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#featured">Featured Projects</a></li>
+            <li><a href="#resume">My Resume</a></li>
+            <li><a class="btn btn-light" href="#">Lets Talk</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="large-12 columns">
+          <ul class="social-icons">
+            <li>
+              <a href="https://www.facebook.com/mike.peters.94">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/mikeio_/">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/mikepetersio">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/mikeio">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github-alt fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="copyright">
+          © 2017 Mike Peters - All&nbsp;Rights&nbsp;Reserved.
+      </div>
+
+    </footer>
+
+  
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/what-input.js"></script>
@@ -79,6 +278,29 @@ $(document).ready(function() {
         $(this).toggleClass('nav-open');
         $( ".overlay" ).fadeToggle();
     });
+});
+
+// Toggle Projects
+$(document).ready(function(){
+    $("#view-all").click(function(){
+        $("#hidden").slideToggle(1000);
+    });
+});
+
+//Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
 </script>
