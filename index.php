e997695511b3d291b4c63b2c150f60dce6249dc0
Mike Peters <mapdesigns@gmail.com>
2017-01-02 17:12:32 -0500

    adjust styles

--- index.php
+++ index.php
@@ -26,40 +26,44 @@
 
 
     <div class="overlay">
-      <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+      <div class="container">
+        <p>Donec lobortis pretium leo et laoreet. Praesent quis commodo neque. Pellentesque ante nulla, feugiat a magna quis, sagittis vehicula eros. Curabitur finibus arcu vitae neque pulvinar, sit amet consequat ligula pharetra. Cras sed mi feugiat, fringilla ex a, viverra odio. Nullam sodales consequat mauris, a vulputate dui fermentum aliquam. Nunc et ligula dignissim massa placerat malesuada sit amet eu magna. Proin feugiat lacus non porta molestie. Curabitur imperdiet venenatis nulla, a bibendum sapien semper eget. Etiam eleifend, mi a fermentum ultricies, eros quam tincidunt erat, id pharetra nisl tortor eget nunc. Vestibulum rhoncus libero ac posuere ultrices. Phasellus elementum velit nec quam mattis, nec semper neque ornare. Suspendisse potenti. Suspendisse vel risus sed elit mattis congue. Ut vestibulum sapien nec nisl euismod tincidunt.</p>
+      </div>
     </div>
 
 
     <header id="top">
 
-      <div class="row">
-        <div class="large-12 columns">
-
-            <div class="nav-mobile">
-              <img src="img/logo.svg" class="logo">
-              <button type="button" class="nav-toggle" aria-label="toggle navigation">
-              <span class="nav-label">Menu</span>
-              <span class="nav-svg">
-                <svg width="54" height="54" version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 524.5 2000 2000.5" enable-background="new 0 524.5 2000 2000.5" xml:space="preserve" stroke-linecap="round">
-                  <path class="x" fill="none" stroke="#000000" stroke-miterlimit="10" d="M1410.5,1235.2H589.3"/>
-                  <path class="x" fill="none" stroke="#000000" stroke-miterlimit="10" d="M1410.5,1815H589.3"/>
-                  <path class="yy" fill="none" stroke="#000000" stroke-miterlimit="10" d="M1410.5,1524.7c0,0-821.2,0-821.2,0c-211.3,0-260,98-218.2,217.6l0,0.1c90.1,260.7,337.6,447.9,628.9,447.9c367.4,0,665.2-297.8,665.2-665.2c0-79-13.8-154.8-39.1-225.2"/>
-                  <path class="yy" fill="none" stroke="#000000" stroke-miterlimit="10" d="M589.3,1524.7c0,0,821.2,0,821.2,0c211.3,0,260-98,218.2-217.6l0-0.1c-90.1-260.7-337.6-447.9-628.9-447.9c-367.4,0-665.2,297.8-665.2,665.2c0,79,13.8,154.8,39.1,225.2"/>
-                </svg>
-              </span>
-              </button>
-            </div>
-
-            <div class="nav-large">
-              <ul>
-                <li><img src="img/logo.svg" class="logo"></li>
-                <li><a href="#about">About Me</a></li>
-                <li><a href="#featured">Featured Projects</a></li>
-                <li><a href="#resume">My Resume</a></li>
-                <li><a class="btn btn-dark" href="#">Let's Talk</a></li>
-              </ul>
-            </div>
-
+      <div class="nav-mobile">
+        <div class="row">
+          <div class="large-12 columns">
+            <img src="img/logo.svg" class="logo">
+            <button type="button" class="nav-toggle" aria-label="toggle navigation">
+            <span class="nav-label">Menu</span>
+            <span class="nav-svg">
+              <svg width="54" height="54" version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 524.5 2000 2000.5" enable-background="new 0 524.5 2000 2000.5" xml:space="preserve" stroke-linecap="round">
+                <path class="x" fill="none" stroke="#000000" stroke-miterlimit="10" d="M1410.5,1235.2H589.3"/>
+                <path class="x" fill="none" stroke="#000000" stroke-miterlimit="10" d="M1410.5,1815H589.3"/>
+                <path class="yy" fill="none" stroke="#000000" stroke-miterlimit="10" d="M1410.5,1524.7c0,0-821.2,0-821.2,0c-211.3,0-260,98-218.2,217.6l0,0.1c90.1,260.7,337.6,447.9,628.9,447.9c367.4,0,665.2-297.8,665.2-665.2c0-79-13.8-154.8-39.1-225.2"/>
+                <path class="yy" fill="none" stroke="#000000" stroke-miterlimit="10" d="M589.3,1524.7c0,0,821.2,0,821.2,0c211.3,0,260-98,218.2-217.6l0-0.1c-90.1-260.7-337.6-447.9-628.9-447.9c-367.4,0-665.2,297.8-665.2,665.2c0,79,13.8,154.8,39.1,225.2"/>
+              </svg>
+            </span>
+            </button>
+          </div>
+        </div>
+        </div>
+      
+      <div class="nav-large">
+        <div class="row">
+          <div class="large-12 columns">
+            <ul>
+              <li><img src="img/logo.svg" class="logo"></li>
+              <li><a href="#about">About Me</a></li>
+              <li><a href="#featured">Featured Projects</a></li>
+              <li><a href="#resume">My Resume</a></li>
+              <li><a class="btn btn-dark" href="#">Let's Talk</a></li>
+            </ul>
+          </div>
         </div>
       </div>
 
@@ -230,37 +234,22 @@
     <footer>
 
       <div class="container">
+        
+        <div class="nav-footer">
+          
+          <div class="row">
+            <div class="large-12 columns">
+              <ul>
+                <li class="logo"><img src="img/logo-alt.svg"></li>
+                <li class="home"><a href="/">Home</a></li>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#featured">Featured Projects</a></li>
+                <li><a href="#resume">My Resume</a></li>
+                <li><a class="btn btn-light" href="#">Let's Talk</a></li>
+              </ul>
+            </div>
+          </div>
 
-        <div class="row">
-          <div class="large-12 columns">
-  
-            <div class="nav-mobile">
-              <ul>
-                <li><a href="#top">Home</a></li>
-                <li><a href="#about">About Me</a></li>
-                <li><a href="#featured">Featured Projects</a></li>
-                <li><a href="#resume">My Resume</a></li>
-                <li><a class="btn btn-light" href="#">Let's Talk</a></li>
-              </ul>
-            </div>
-  
-          </div>
-        </div>
-  
-        <div class="row">
-          <div class="large-12 columns">
-  
-            <div class="nav-large">
-              <ul>
-                <li><img src="img/logo-alt.svg" class="logo"></li>
-                <li><a href="#about">About Me</a></li>
-                <li><a href="#featured">Featured Projects</a></li>
-                <li><a href="#resume">My Resume</a></li>
-                <li><a class="btn btn-light" href="#">Let's Talk</a></li>
-              </ul>
-            </div>
-  
-          </div>
         </div>
   
         <div class="row">
@@ -320,48 +309,47 @@
     <script src="js/app.js"></script>
 
     <script>
-// Toggle Overlay
-$(document).ready(function() {
-    $(".nav-toggle").click(function() {
-        $("#logo").fadeToggle();
-        $('body').toggleClass('no-scroll');
-        $(this).toggleClass('nav-open');
-        $( ".overlay" ).fadeToggle();
-    });
-});
-
-// Toggle Projects
-$(document).ready(function(){
-    $("#view-all").click(function(){
-        $("#hidden").slideToggle(1000);
-    });
-});
-
-$(function(){
-   $("#view-all").click(function () {
-      $(this).text(function(i, text){
-          return text === "View All Projects" ? "View Less" : "View All Projects";
+      // Toggle Overlay
+      $(document).ready(function() {
+          $(".nav-toggle").click(function() {
+              $("#logo").fadeToggle();
+              $('body').toggleClass('no-scroll');
+              $(this).toggleClass('nav-open');
+              $( ".overlay" ).fadeToggle();
+          });
+      });
+      
+      // Toggle Projects
+      $(document).ready(function(){
+          $("#view-all").click(function(){
+              $("#hidden").slideToggle(1000);
+          });
+      });
+      
+      $(function(){
+         $("#view-all").click(function () {
+            $(this).text(function(i, text){
+                return text === "View All Projects" ? "View Less" : "View All Projects";
+            })
+         });
       })
-   });
-})
-
-//Smooth Scroll
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-</script>
+      
+      //Smooth Scroll
+      $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+  </script>
 
   </body>
 </html>
