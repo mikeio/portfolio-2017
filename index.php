64c8a0c4e53c319c631ad567737660995a3b88c2
Mike Peters <mapdesigns@gmail.com>
2017-01-19 17:48:01 -0500

    optimize nav-footer

--- index.php
+++ index.php
@@ -72,24 +72,26 @@
 
       <div class="row nav-header">
         <div class="large-12 columns">
-          <a href="/"><img src="img/logo.svg" class="logo"></a>
-          <a class="btn btn-dark" href="mailto:mapdesigns@gmail.com?subject=Hello">Let's Talk</a>
-          <button type="button" class="nav-toggle" aria-label="toggle navigation">
-            <span class="nav-label">Menu</span>
-            <span class="nav-svg">
-              <svg version="1.1" id="hamburger" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
-                <style type="text/css">.x,.yy{fill:none;stroke:#000000;stroke-linecap:round;stroke-miterlimit:10;}</style>
-                <g><path class="x" d="M31.4,12H8.6"/></g>
-                <g><path class="x" d="M31.4,28.1H8.6"/></g>
-                <path class="yy" d="M31.4,20H8.6c-5.9,0-7.2,2.7-6.1,6v0C5,33.3,11.9,38.5,20,38.5c10.2,0,18.5-8.3,18.5-18.5c0-2.2-0.4-4.3-1.1-6.3"/>
-                <path class="yy" d="M8.6,20h22.8c5.9,0,7.2-2.7,6.1-6v0C35,6.7,28.1,1.5,20,1.5C9.8,1.5,1.5,9.8,1.5,20c0,2.2,0.4,4.3,1.1,6.3"/>
-              </svg>
-            </span>
-          </button>
           <ul>
+            <li><a href="/"><img src="img/logo.svg" class="logo"></a></li>
             <li><a href="#about">About Me</a></li>
             <li><a href="#featured">Featured Projects</a></li>
             <li><a href="#resume">My Resume</a></li>
+            <li><a class="btn btn-dark" href="mailto:mapdesigns@gmail.com?subject=Hello">Let's Talk</a></li>
+            <li>
+              <button type="button" class="nav-toggle" aria-label="toggle navigation">
+                <span class="nav-label">Menu</span>
+                <span class="nav-svg">
+                  <svg version="1.1" id="hamburger" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+                    <style type="text/css">.x,.yy{fill:none;stroke:#000000;stroke-linecap:round;stroke-miterlimit:10;}</style>
+                    <g><path class="x" d="M31.4,12H8.6"/></g>
+                    <g><path class="x" d="M31.4,28.1H8.6"/></g>
+                    <path class="yy" d="M31.4,20H8.6c-5.9,0-7.2,2.7-6.1,6v0C5,33.3,11.9,38.5,20,38.5c10.2,0,18.5-8.3,18.5-18.5c0-2.2-0.4-4.3-1.1-6.3"/>
+                    <path class="yy" d="M8.6,20h22.8c5.9,0,7.2-2.7,6.1-6v0C35,6.7,28.1,1.5,20,1.5C9.8,1.5,1.5,9.8,1.5,20c0,2.2,0.4,4.3,1.1,6.3"/>
+                  </svg>
+                </span>
+              </button>
+            </li>
           </ul>
         </div>
       </div>
@@ -259,9 +261,8 @@
         
           <div class="row nav-footer">
             <div class="large-12 columns">
-              <a href="/"><img src="img/logo-alt.svg" class="logo"></a>
-              <a class="btn btn-light" href="mailto:mapdesigns@gmail.com?subject=Hello">Let's Talk</a>
               <ul>
+                <li><a href="/"><img src="img/logo-alt.svg" class="logo"></a>
                 <li><a href="#about">Home</a></li>
                 <li><a href="#about">About Me</a></li>
                 <li><a href="#featured">Featured Projects</a></li>
