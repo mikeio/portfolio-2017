d934638b3892e22a85e8bb98e41f4e028e330537
Mike Peters <mapdesigns@gmail.com>
2017-02-03 13:48:05 -0500

    add wow.js

--- js/scripts.js
+++ js/scripts.js
@@ -34,20 +34,18 @@ $(function() {
   });
 });
 
-window.sr = ScrollReveal();
-  sr.reveal('.fadeInLeft', { 
-    mobile: false, 
-    viewFactor: 0.7, 
-    distance: '50px',
-    scale: 1, 
-    duration: 1950, 
-    origin: 'left', 
-    easing: 'cubic-bezier(.18,1,.21,1)' });
-  sr.reveal('.fadeInRight', { 
-    mobile: false, 
-    viewFactor: 0.7, 
-    distance: '50px',
-    scale: 1, 
-    duration: 1950, 
-    origin: 'right', 
-    easing: 'cubic-bezier(.18,1,.21,1)' });
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       100,          // distance to the element when triggering the animation (default is 0)
+    mobile:       false,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
