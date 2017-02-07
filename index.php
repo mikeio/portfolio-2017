d73ee9358a9e8a8821c11fec9f63a0682b45d20d
Mike Peters <mapdesigns@gmail.com>
2017-02-07 16:54:05 -0500

    add icons / rename overlay

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   </head>
   <body>
 
-    <?php include 'mobile-nav.php';?>
+    <?php include 'mobile-overlay.php';?>
 
     <header id="top">
 
@@ -35,16 +35,26 @@
 
       <div class="row">
         <div class="large-12 columns">
-          <img data-interchange="[img/books.png, small], [img/books.png, medium], [img/books@2x.png, large]">
+          <img data-interchange="[img/books.png, small], [img/books.png, medium], [img/books@2x.png, large]" class="books">
           <img src="img/playfair-type.svg" class="playfair">
           <h4>Various Aspects</h4>
           <h1>An eye for innovation and a drive to create beautiful & simple solutions.</h1>
           <p>I’m a highly-motivated and dedicated professional with almost a decade of commercial design experience. This experience together with freelance work I've done, has allowed me to acquire a wide range of skills as a designer. Building products from the start, working side by side with developers and learning how users interact with my designs is what I love the most about my job. I believe good design is a simple and achievable experience that differs with each and every project I work on.</p>
+
+          <div class="my-expertise" id="expertise">
+            <ul class="icons">
+              <li><img src="img/conception.svg"><br><span>Conception</span></li>
+              <li><img src="img/webdesign.svg"><br><span>Web Design</span></li>
+              <li><img src="img/uidesign.svg"><br><span>UI Design</span></li>
+              <li><img src="img/ux.svg"><br><span>UX Design</span></li>
+              <li><img src="img/development.svg"><br><span>Development</span></li>
+            </ul>
+          </div>
+
         </div>
       </div>
 
     </section>
-
 
     <section class="featured-projects" id="featured">
 
