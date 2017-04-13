4e3dcfdd79ae6b36e05451c23e821e055a37d338
Mike Peters <mapdesigns@gmail.com>
2017-04-13 19:18:37 -0400

    initial commit

---
+++ scripts.php
@@ -0,0 +1,9 @@
+<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/jquery-ui.js"></script>
+
+    <!-- Custom Scripts -->
+    <script src="js/wow.min.js"></script>
+    <script src="js/scripts.js"></script>
