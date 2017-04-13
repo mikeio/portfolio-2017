e6b6f87f2481238de7773b952dcb1598dddc05f4
Mike Peters <mapdesigns@gmail.com>
2017-04-13 19:18:31 -0400

    change links to fadeInDown / adjust timing

--- mobile-overlay.php
+++ mobile-overlay.php
@@ -6,31 +6,31 @@
           <h4>Navigation</h4>
           <nav>
             <ul class="nav">
-              <li class="wow fadeInUp" data-wow-delay=".5s"><a href="/">Home</a></li>
-              <li class="wow fadeInUp" data-wow-delay=".6s"><a href="/#about">About Me</a></li>
-              <li class="wow fadeInUp" data-wow-delay=".7s"><a href="/#featured">Featured Projects</a></li>
-              <li class="wow fadeInUp" data-wow-delay=".8s"><a href="/#resume">My Resume</a></li>
-              <li class="wow fadeInUp" data-wow-delay=".9s"><a href="mailto:mapdesigns@gmail.com?subject=Hello">Let's Talk</a></li>
+              <li class="wow fadeInDown" data-wow-delay=".5s"><a href="/">Home</a></li>
+              <li class="wow fadeInDown" data-wow-delay=".6s"><a href="/#about">About Me</a></li>
+              <li class="wow fadeInDown" data-wow-delay=".7s"><a href="/#featured">Featured Projects</a></li>
+              <li class="wow fadeInDown" data-wow-delay=".8s"><a href="/#resume">My Resume</a></li>
+              <li class="wow fadeInDown" data-wow-delay=".9s"><a href="mailto:mapdesigns@gmail.com?subject=Hello">Let's Talk</a></li>
             </ul>
           </nav>
 
           <ul class="social-icons">
-            <li class="wow fadeIn" data-wow-delay="1s">
+            <li class="wow fadeIn" data-wow-delay="1.5s">
               <a href="https://www.facebook.com/mike.peters.94">
                   <i class="fa fa-facebook"></i>
               </a>
             </li>
-            <li class="wow fadeIn" data-wow-delay="1.1s">
+            <li class="wow fadeIn" data-wow-delay="1.6s">
               <a href="https://www.instagram.com/mikeio_/">
                   <i class="fa fa-instagram"></i>
               </a>
             </li>
-            <li class="wow fadeIn" data-wow-delay="1.2s">
+            <li class="wow fadeIn" data-wow-delay="1.7s">
               <a href="https://www.linkedin.com/in/mikepetersio">
                   <i class="fa fa-linkedin"></i>
               </a>
             </li>
-            <li class="wow fadeIn" data-wow-delay="1.3s">
+            <li class="wow fadeIn" data-wow-delay="1.8s">
               <a href="https://github.com/mikeio">
                   <i class="fa fa-github-alt"></i>
               </a>
