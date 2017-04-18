70e95c2174ffda643bb7a4acfeef06bc5428d3b4
Mike Peters <mapdesigns@gmail.com>
2017-04-18 19:31:43 -0400

    initial commit

---
+++ single.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Online Portfolio of Mike Peters</title>
+
+    <?php include 'head.php';?>
+    
+  </head>
+  <body>
+
+    <?php include 'mobile-overlay.php';?>
+
+    <header id="top">
+
+      <?php include 'nav-header.php';?>
+
+      <div class="row">
+        <div class="large-12 columns">
+
+          <div class="project-title">
+            <h1>Honda Dealers</h1>
+            <h4>Hi! My name is Mike&nbsp;Peters</h4>
+          </div>
+
+        </div>
+      </div>
+
+    </header>
+
+
+    <section class="project-info">
+    
+      <div class="img-divide">
+        <img src="img/honda01.png">
+      </div>
+
+      <div class="row">
+        <div class="large-12 columns">
+          <ul>
+            <li><h4>Subtitle</h4><h1>Title</h1></li>
+            <li><h4>Subtitle</h4><h1>Title</h1></li>
+            <li><h4>Subtitle</h4><h1>Title</h1></li>
+          </ul>
+          <p>I am a dedicated and highly-motivated web professional with almost a decade of experience. I enjoy designing beautiful and functional user experiences while also writing clean and efficient front-end code. I believe good design is always in service of the user and it's my job to ensure that a project achieves both its business objectives and the needs of its users above all else.</p>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="project-full">
+      <ul>
+        <li><img src="img/honda02.jpg"></li>
+        <li><img src="img/honda03.jpg"></li>
+        <li><img src="img/honda04.jpg"></li>
+        <li><img src="img/honda05.jpg"></li>
+      </ul>
+    </section>
+
+    <?php include 'cta.php';?>
+
+    <?php include 'footer.php';?>
+
+    <?php include 'scripts.php';?>
+
+  </body>
+</html>
