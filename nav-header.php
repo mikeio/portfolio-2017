7b917f39ce685fa0d584b6b465be9014ee303345
Mike Peters <mapdesigns@gmail.com>
2017-04-21 19:58:12 -0400

    correct the links

--- nav-header.php
+++ nav-header.php
@@ -3,9 +3,9 @@
           <div class="large-12 columns">
             <ul>
               <li><a href="/"><img src="img/logo.svg" class="logo"></a></li>
-              <li><a href="#about">About Me</a></li>
-              <li><a href="#featured">Featured Projects</a></li>
-              <li><a href="#resume">My Resume</a></li>
+              <li><a href="/#about">About Me</a></li>
+              <li><a href="/#featured">Featured Projects</a></li>
+              <li><a href="/#resume">My Resume</a></li>
               <li><a class="btn btn-dark" href="mailto:mapdesigns@gmail.com?subject=Hello">Let's Talk</a></li>
               <li>
                 <div class="burger">
