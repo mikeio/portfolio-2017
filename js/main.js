a64c43e98f993084b2061b2951b7853d78a5f3a7
Mike Peters <mapdesigns@gmail.com>
2017-07-12 16:27:24 -0400

    minify js

--- js/main.js
+++ js/main.js
@@ -16,70 +16,103 @@ $(document).ready(function() {
             'slow');
     });
 
-});
-
-var wow = new WOW({
-    boxClass: 'wow', // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset: 0, // distance to the element when triggering the animation (default is 0)
-    mobile: true, // trigger animations on mobile devices (default is true)
-    live: true, // act on asynchronously loaded content (default is true)
-    callback: function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null // optional scroll container selector, otherwise use window
-});
-wow.init();
-
-(function($) {
-    var $window = $(window),
-        $html = $('.logo, .intro, .work ul li');
-
-    function resize() {
-        if ($window.width() < 1024) {
-            return $html.addClass('disable-wow');
-        }
-
-        $html.removeClass('disable-wow');
+    setRandomClass1();
+    setInterval(function () {
+        setRandomClass1();
+    }, 7000);
+    
+    function setRandomClass1() {
+        var items = $("#initial ul li .container");
+        var number = items.length;
+        var random = Math.floor((Math.random() * number));
+        items.removeClass("active");
+        items.eq(random).addClass("active");
     }
 
-    $window
-        .resize(resize)
-        .trigger('resize');
-})(jQuery);
-
-$('#view-all').click(function() {
-    if ($(this).text() === 'View More') {
-        $(this).text('View Less');
-    } else {
-        $(this).text('View More');
+    setRandomClass2();
+    setInterval(function () {
+        setRandomClass2();
+    }, 7000);
+    
+    function setRandomClass2() {
+        var items = $("#show-more ul li .container");
+        var number = items.length;
+        var random = Math.floor((Math.random() * number));
+        items.removeClass("active");
+        items.eq(random).addClass("active");
     }
-});
 
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1200);
-                return false;
+    (function($) {
+        var $window = $(window),
+            $html = $('.logo, .intro, .work ul li');
+    
+        function resize() {
+            if ($window.width() < 1024) {
+                return $html.addClass('disable-wow');
             }
+    
+            $html.removeClass('disable-wow');
+        }
+    
+        $window
+            .resize(resize)
+            .trigger('resize');
+    })(jQuery);
+
+    $('#view-all').click(function() {
+        if ($(this).text() === 'View More') {
+            $(this).text('View Less');
+        } else {
+            $(this).text('View More');
         }
     });
+    
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1200);
+                    return false;
+                }
+            }
+        });
+    });
+    
+    const elements = document.querySelectorAll('[data-chaffle]');
+    Array.prototype.forEach.call(elements, function(el) {
+        const chaffle = new Chaffle(el, {
+            lang: 'en',
+            speed: 100, // default: 20
+            delay: 400, // default: 100
+        });
+        el.addEventListener('mouseover', function() {
+            chaffle.init();
+        });
+    });
+
 });
 
-const elements = document.querySelectorAll('[data-chaffle]');
-Array.prototype.forEach.call(elements, function(el) {
-    const chaffle = new Chaffle(el, {
-        lang: 'en',
-        speed: 100, // default: 20
-        delay: 400, // default: 100
+
+$( window ).load(function() {
+  $("#loading img").fadeOut(600);
+  $("#loading").delay(300).fadeOut(600);
+  var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 0, // distance to the element when triggering the animation (default is 0)
+        mobile: true, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
     });
-    el.addEventListener('mouseover', function() {
-        chaffle.init();
-    });
+    wow.init();
 });
+
+
